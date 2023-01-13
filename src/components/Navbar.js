23b18e8e9f597da7ffbd65f38599d8dd562138b8
Jay <jdragon2050@gmail.com>
2023-01-13 14:01:10 -0800

    add navbar and import styled components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-function App() {
-  return <div>stuff</div>;
-}
-
-export default App;
+export const Navbar = () => {
+  return <div>Navbar</div>;
+};
