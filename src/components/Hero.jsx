e27450359420356e40e120cd2cf6abaf715e11ec
Jay <jdragon2050@gmail.com>
2023-01-17 08:33:24 -0800

    add changes to hero.jsx for more stuff on the homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <Wrapper>
+      <article>
+        <h1>
+          Get your <br />
+          first pokemon cards
+        </h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima dolor
+          libero repellat porro aliquam delectus officiis labore excepturi
+          facere! Repellendus in nostrum repellat asperiores sit adipisci
+          dolorum harum optio recusandae.
+        </p>
+      </article>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.section``;
 
 export default Hero;
