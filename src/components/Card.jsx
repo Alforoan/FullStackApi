f1efcd8f6f7cfd211f05280d53d3b2c0fe08fe83
Jay <jdragon2050@gmail.com>
2023-01-25 23:10:52 -0800

    add successfully destructure featured cards on homepage

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
-function Card() {
-  return <div>Card</div>;
+import styled from "styled-components";
+function Card({
+  id,
+  name,
+  images: { large: image },
+  cardmarket: {
+    prices: { averageSellPrice: price },
+  },
+}) {
+  return (
+    <Wrapper>
+      <div>
+        <h3>{name}</h3>
+        <img src={image} alt={name} />
+      </div>
+      <footer>
+        <p>{price}</p>
+      </footer>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div``;
 
 export default Card;
