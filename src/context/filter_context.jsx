3a4096e955dcb5ee40ae7d8ed86355b894e41fa7
Jay <jdragon2050@gmail.com>
2023-01-29 12:06:03 -0800

    add initialState and others in filter_context.jsx

--- src/context/filter_context.jsx
+++ src/context/filter_context.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  LOAD_CARDS,
+  SET_LISTVIEW,
+  SET_GRIDVIEW,
+  UPDATE_SORT,
+  SORT_CARDS,
+  UPDATE_FILTERS,
+  FILTER_CARDS,
+  CLEAR_FILTERS,
+} from "../actions";
 
-function filter_context() {
-  return <div>filter_context</div>;
+const initialState = {
+  filtered_cards: [],
+  all_cards: [],
+};
+
+const FilterContext = React.createContext();
+
+function filter_context({ children }) {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+  return (
+    <FilterContext.Provider value="filter context">
+      {children}
+    </FilterContext.Provider>
+  );
 }
 
-export default filter_context;
+export const useFilterContext = () => {
+  return React.useContext(FilterContext);
+};
