3c0d035a507fac920f06ab7e2d5b9c822b381800
Jay <jdragon2050@gmail.com>
2023-01-28 23:23:09 -0800

    fix some errors and add increase and decrease functions within addtocart component

--- src/components/AddtoCart.jsx
+++ src/components/AddtoCart.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function AddtoCart() {
-  const [amount, setAmount] = useState(1);
+  const [amount, setAmount] = React.useState(1);
 
+  function Increase() {}
+  function Decrease() {}
   return (
     <Wrapper>
       <div>
