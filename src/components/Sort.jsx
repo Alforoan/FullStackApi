b638ee315754befd5a978ffb81f632b35733153a
Jay <jdragon2050@gmail.com>
2023-01-30 00:37:26 -0800

    add buttons for grid and list view as well as add some css to the buttons

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import styled from "styled-components";
+import { useFilterContext } from "../context/filter_context";
+import { BsFillGridFill, BsList } from "react-icons/bs";
 
 function Sort() {
-  return <div>Sort</div>;
+  const { filtered_cards: cards, grid_view } = useFilterContext();
+  return (
+    <Wrapper>
+      <div className="btn-container">
+        <button type="button" className={`${grid_view && "active"}`}>
+          <BsFillGridFill />
+        </button>
+        <button type="button" className={`${!grid_view && "active"}`}>
+          <BsList />
+        </button>
+      </div>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.section`
+  .btn-container {
+    display: flex;
+    button {
+      background: transparent;
+      border: 1px solid black;
+      color: black;
+      width: 1.5rem;
+      border-radius: 0.25rem;
+      height: 1.5rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      svg {
+        font-size: 1rem;
+      }
+    }
+    .active {
+      background: black;
+      color: white;
+    }
+  }
+`;
 
 export default Sort;
