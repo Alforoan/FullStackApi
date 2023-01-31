cc80105932144ffdccc73cae55a309d3a94bdb8c
Jay <jdragon2050@gmail.com>
2023-01-30 17:21:11 -0800

    add destructure and set up the filter function

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+import { useFilterContext } from "../context/filter_context";
 
 function Filters() {
-  return <div>Filters</div>;
+  const {
+    filters: { text, category, min_price, max_price, price, types },
+    updateFilters,
+    clearFilters,
+    all_cards,
+  } = useFilterContext();
+  return (
+    <Wrapper>
+      <div className="content">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="form-control">
+            <input
+              type="text"
+              name="text"
+              placeholder="search"
+              value={text}
+              onChange={updateFilters}
+            />
+          </div>
+        </form>
+      </div>
+    </Wrapper>
+  );
 }
 
+const Wrapper = styled.section``;
 export default Filters;
