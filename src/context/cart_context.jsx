3b07607888422ac2bf041511f3766113679042f3
Jay <jdragon2050@gmail.com>
2023-01-31 22:48:25 -0800

    add a few files and fix bug of added item not appearing

--- src/context/cart_context.jsx
+++ src/context/cart_context.jsx
@@ -9,17 +9,27 @@ import {
   COUNT_CART_TOTALS,
 } from "../actions";
 
-const initialState = {};
+const initialState = {
+  cart: [],
+  total_items: 0,
+  total_amount: 0,
+  tax_fee: 0,
+};
 
 const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = React.useReducer(reducer, initialState);
-  const addToCart = (id, color, amount, product) => {
-    dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
+  const addToCart = (id, amount, card) => {
+    dispatch({ type: ADD_TO_CART, payload: { id, amount, card } });
   };
+  const removeItem = (id) => {};
+  const toggleAmount = (id, value) => {};
+  const clearCart = () => {};
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   );
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useCartContext } from "../context/cart_context";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import PageHero from "../components/PageHero";
+import CartContent from "../components/CartContent";
 
 function CartPage() {
-  return <div>CartPage</div>;
+  const { cart } = useCartContext();
+  if (cart.length < 1) {
+    return (
+      <Wrapper className="page-100">
+        <div className="empty">
+          <h2>Your cart is empty</h2>
+          <Link to="/cards" className="btn">
+            Fill it
+          </Link>
+        </div>
+      </Wrapper>
+    );
+  }
+  return (
+    <main>
+      <PageHero title="cart" />
+      <Wrapper className="page">
+        <CartContent />
+      </Wrapper>
+    </main>
+  );
 }
+
+const Wrapper = styled.main``;
 
 export default CartPage;
