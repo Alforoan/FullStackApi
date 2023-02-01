070bf896e716ef3a5d5218e6ff5e36a4b808d8dd
Jay <jdragon2050@gmail.com>
2023-01-31 23:03:08 -0800

    add filed necessary for the cart page and set up the cartcontent.jsx

---
+++ src/components/CartColumns.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CartColumns() {
+  return <div>CartColumns</div>;
+}
+
+export default CartColumns;
--- src/components/CartContent.jsx
+++ src/components/CartContent.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
+import { useCartContext } from "../context/cart_context";
+import CartColumns from "./CartColumns";
+import CartItem from "./CartItem";
+import { Link } from "react-router-dom";
+import CartTotal from "./CartTotal";
 
 function CartContent() {
-  return <div>CartContent</div>;
+  const { cart, clearCart } = useCartContext();
+  return (
+    <Wrapper>
+      <CartColumns />
+      {cart.map((item) => {
+        return <CartItem key={item.id} {...item} />;
+      })}
+      <hr />
+      <div className="link-container">
+        <Link to="/cards" className="link-btn">
+          Continue Shopping
+        </Link>
+      </div>
+      <button type="button" className="link-btn clear-btn" onClick={clearCart}>
+        Clear Shopping Cart
+      </button>
+      <CartTotal />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.section``;
 
 export default CartContent;
