d09c25ac435c2d84eb1c165c632c0b7cd52d74e5
Jay <jdragon2050@gmail.com>
2023-01-31 22:53:46 -0800

    add localstorage logic

--- src/context/cart_context.jsx
+++ src/context/cart_context.jsx
@@ -9,8 +9,17 @@ import {
   COUNT_CART_TOTALS,
 } from "../actions";
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   tax_fee: 0,
@@ -26,6 +35,10 @@ export const CartProvider = ({ children }) => {
   const removeItem = (id) => {};
   const toggleAmount = (id, value) => {};
   const clearCart = () => {};
+
+  React.useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
   return (
     <CartContext.Provider
       value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
