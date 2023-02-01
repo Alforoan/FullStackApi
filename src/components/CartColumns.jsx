0ed0bb91fc1dbe828c74980fda6c2c57192ea648
Jay <jdragon2050@gmail.com>
2023-01-31 23:08:47 -0800

    add h5's to cartcolumns and some css

--- src/components/CartColumns.jsx
+++ src/components/CartColumns.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 
 function CartColumns() {
-  return <div>CartColumns</div>;
+  return (
+    <Wrapper>
+      <div className="content">
+        <h5>Item</h5>
+        <h5>Price</h5>
+        <h5>Quantity</h5>
+        <h5>Subtotal</h5>
+        <span></span>
+      </div>
+      <hr />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.section`
+  .content {
+    display: flex;
+
+    justify-content: center;
+    column-gap: 5rem;
+    h5 {
+      color: var(lightgray);
+      font-weight: 400;
+    }
+  }
+`;
 
 export default CartColumns;
