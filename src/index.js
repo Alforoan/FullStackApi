60c9ab413fe9e50228629b0e1f06b5f0c5617c72
Jay <jdragon2050@gmail.com>
2023-02-01 21:41:05 -0800

    add auth0provider

--- src/index.js
+++ src/index.js
@@ -7,20 +7,31 @@ import { BrowserRouter } from "react-router-dom";
 import CardsProvider from "./context/cards_context";
 import FilterProvider from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+//dev-mkhytkbfzkgtvzuc.us.auth0.com
+//7Tdo43MfXETURqE7o7nl1uhdA5CxQ8Bk
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <CardsProvider>
-        <FilterProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </FilterProvider>
-      </CardsProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-mkhytkbfzkgtvzuc.us.auth0.com"
+    clientId="7Tdo43MfXETURqE7o7nl1uhdA5CxQ8Bk"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <React.StrictMode>
+      <BrowserRouter>
+        <CardsProvider>
+          <FilterProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </FilterProvider>
+        </CardsProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
