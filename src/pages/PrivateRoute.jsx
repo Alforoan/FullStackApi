ca555679d2609aee3279b9845472ff778b66f435
Jay <jdragon2050@gmail.com>
2023-02-04 00:18:19 -0800

    add navigate so that if user is not logged in, user's not able to visit checkout page

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useUserContext } from "../context/user_context";
+import { Route, Navigate } from "react-router-dom";
 
-function PrivateRoute() {
-  return <div>PrivateRoute</div>;
+function PrivateRoute({ children, ...rest }) {
+  const { myUser } = useUserContext();
+  return (
+    <div>
+      {!myUser ? <Navigate to="/" replace={true} /> : null}
+      <h1>hello</h1>
+    </div>
+  );
 }
 
 export default PrivateRoute;
+{
+  /* <Route
+      {...rest}
+      render={() => {
+        return myUser ? children : <Navigate to="/"></Navigate>;
+      }}
+    >
+      Privatemain
+    </Route> */
+}
