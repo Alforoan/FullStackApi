01f87eb696a5383b7a45fc3a55b610845eedbe25
Jay <jdragon2050@gmail.com>
2023-02-05 00:47:33 -0800

    add payment form and other logic relevant to payment

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-// import { useUserContext } from "../context/user_context";
+
 import { Navigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useCartContext } from "../context/cart_context";
+
 import { Outlet } from "react-router-dom";
 
 function PrivateRoute() {
